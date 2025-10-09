9d511c162f386e37b10f9abd0a1e4355be2fdb56
Victor Manuel <victormanuelgama21@gmail.com>
2025-10-08 22:46:44 -0300

    Create RegistrationTokenRepository.java

---
+++ src/main/java/com/inter/SistemaDeVisitas/repo/RegistrationTokenRepository.java
@@ -0,0 +1,10 @@
+package com.inter.SistemaDeVisitas.repo;
+
+import com.inter.SistemaDeVisitas.entity.RegistrationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RegistrationTokenRepository extends JpaRepository<RegistrationToken, Long> {
+    Optional<RegistrationToken> findByToken(String token);
+}
