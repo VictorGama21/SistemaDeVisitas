6111c04d209504c790e52a30e3c0373b94901055
Victor Manuel <victormanuelgama21@gmail.com>
2025-10-08 23:28:09 -0300

    Update RegistrationTokenRepository.java

--- src/main/java/com/inter/SistemaDeVisitas/repo/RegistrationTokenRepository.java
+++ src/main/java/com/inter/SistemaDeVisitas/repo/RegistrationTokenRepository.java
@@ -1,3 +1,4 @@
+// src/main/java/com/inter/SistemaDeVisitas/repo/RegistrationTokenRepository.java
 package com.inter.SistemaDeVisitas.repo;
 
 import com.inter.SistemaDeVisitas.entity.RegistrationToken;
@@ -6,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RegistrationTokenRepository extends JpaRepository<RegistrationToken, Long> {
-    Optional<RegistrationToken> findByToken(String token);
+  Optional<RegistrationToken> findByToken(String token);
 }
