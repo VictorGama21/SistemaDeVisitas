2932aa59e85f45583a8455d517b3d4027a16ab3e
Victor Manuel <victormanuelgama21@gmail.com>
2025-10-12 16:24:59 -0300

    Create StoreRepository.java

---
+++ src/main/java/com/inter/SistemaDeVisitas/repo/StoreRepository.java
@@ -0,0 +1,7 @@
+package com.inter.SistemaDeVisitas.repo;
+
+import com.inter.SistemaDeVisitas.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
