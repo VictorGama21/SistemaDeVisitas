24f4c15db9c59881d22cbc4ca38f1d36158c33e2
Victor Manuel <victormanuelgama21@gmail.com>
2025-10-12 20:45:14 -0300

    Update StoreRepository.java

--- src/main/java/com/inter/SistemaDeVisitas/repo/StoreRepository.java
+++ src/main/java/com/inter/SistemaDeVisitas/repo/StoreRepository.java
@@ -3,5 +3,11 @@ package com.inter.SistemaDeVisitas.repo;
 import com.inter.SistemaDeVisitas.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    List<Store> findByActiveTrueOrderByNameAsc();
+    List<Store> findByActiveFalseOrderByNameAsc();
+    List<Store> findAllByOrderByNameAsc();
+    long countByActiveTrue();
 }
