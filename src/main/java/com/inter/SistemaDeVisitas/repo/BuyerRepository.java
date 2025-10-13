73315eb13b0dd75801bb29f76cc8b007c358a6ea
Victor Manuel <victormanuelgama21@gmail.com>
2025-10-12 22:21:20 -0300

    Create BuyerRepository.java

---
+++ src/main/java/com/inter/SistemaDeVisitas/repo/BuyerRepository.java
@@ -0,0 +1,13 @@
+package com.inter.SistemaDeVisitas.repo;
+
+import com.inter.SistemaDeVisitas.entity.Buyer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BuyerRepository extends JpaRepository<Buyer, Long> {
+  List<Buyer> findByActiveTrueOrderByNameAsc();
+  Optional<Buyer> findByNameIgnoreCase(String name);
+  List<Buyer> findAllByOrderByNameAsc();
+}
