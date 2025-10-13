7586f3a53f1ec538ed40eeae76440509946820e8
Victor Manuel <victormanuelgama21@gmail.com>
2025-10-12 22:22:38 -0300

    Create SegmentRepository.java

---
+++ src/main/java/com/inter/SistemaDeVisitas/repo/SegmentRepository.java
@@ -0,0 +1,13 @@
+package com.inter.SistemaDeVisitas.repo;
+
+import com.inter.SistemaDeVisitas.entity.Segment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SegmentRepository extends JpaRepository<Segment, Long> {
+  List<Segment> findByActiveTrueOrderByNameAsc();
+  Optional<Segment> findByNameIgnoreCase(String name);
+  List<Segment> findAllByOrderByNameAsc();
+}
