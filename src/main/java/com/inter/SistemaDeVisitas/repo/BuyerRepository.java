848e0f82b908a82c20b5b1b2092435d990bea4ec
Victor Manuel <victormanuelgama21@gmail.com>
2025-10-14 22:44:45 -0300

    Update BuyerRepository.java

--- src/main/java/com/inter/SistemaDeVisitas/repo/BuyerRepository.java
+++ src/main/java/com/inter/SistemaDeVisitas/repo/BuyerRepository.java
@@ -1,6 +1,8 @@
 package com.inter.SistemaDeVisitas.repo;
 
 import com.inter.SistemaDeVisitas.entity.Buyer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,5 @@ public interface BuyerRepository extends JpaRepository<Buyer, Long> {
   List<Buyer> findByActiveTrueOrderByNameAsc();
   Optional<Buyer> findByNameIgnoreCase(String name);
   List<Buyer> findAllByOrderByNameAsc();
+  Page<Buyer> findByNameContainingIgnoreCaseOrderByNameAsc(String name, Pageable pageable);
 }
