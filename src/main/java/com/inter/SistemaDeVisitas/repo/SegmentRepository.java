59b74bfa2ae7b415d69a661f0b5554149329a568
Victor Manuel <victormanuelgama21@gmail.com>
2025-10-14 22:45:11 -0300

    Update SegmentRepository.java

--- src/main/java/com/inter/SistemaDeVisitas/repo/SegmentRepository.java
+++ src/main/java/com/inter/SistemaDeVisitas/repo/SegmentRepository.java
@@ -1,6 +1,8 @@
 package com.inter.SistemaDeVisitas.repo;
 
 import com.inter.SistemaDeVisitas.entity.Segment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,5 @@ public interface SegmentRepository extends JpaRepository<Segment, Long> {
   List<Segment> findByActiveTrueOrderByNameAsc();
   Optional<Segment> findByNameIgnoreCase(String name);
   List<Segment> findAllByOrderByNameAsc();
+  Page<Segment> findByNameContainingIgnoreCaseOrderByNameAsc(String name, Pageable pageable);
 }
